3d2616accbae78138e8bc9ba4670f8ade3969732
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-03-28 17:08:01 +1300

    Final deployment to Heroku prep

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // app.js
-
+require('dotenv').config({ path: './config.env'});
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 var cors = require('cors');
+
 
 // routes
 const books = require('./routes/api/books');
@@ -18,11 +20,21 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/my-book-app/build')));
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, 'my-book-app', 'build', 'index.html'));
+    });
+}else{
+    //app.get('/', (req, res) => res.send('Hello world! development builds'));
+    app.get('/', (req, res) => {res.send("Api Running -  Dev Build")});
+}
+
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
